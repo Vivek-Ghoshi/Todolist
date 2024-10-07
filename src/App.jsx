310dce2a954d7e74e6696f4a476c6af67db6e883
Vivek ghoshi <the.vivekghoshi@gmail.com>
2024-10-07 21:55:55 +0530

    started react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+const App = () => {
+  return (
+   
+   <div className="mt-[7%] w-[25%] h-[20%] flex rounded-3xl border justify-around items-center ">
+     <div className="text-yellow-100">
+      <h1 className="text-3xl font-bold">Let's ToDo</h1>
+      <p>keep doing things</p>
+     </div>
+     <div>
+      0/0
+     </div>
+
+
+
+   </div>
+   
+  )
+}
+
+export default App
