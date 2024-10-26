b7a514718fad2f3d03fc1c073f752138ae345568
Vivek ghoshi <the.vivekghoshi@gmail.com>
2024-10-26 14:11:35 +0530

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,87 @@
+import React, { useState } from "react";
 
 const App = () => {
+  const [tasks, settasks] = useState([]);
+  const [title, settitle] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const newtask = {title, completed: false}
+
+    const copytask = [...tasks];
+    copytask.push(newtask);
+    settasks(copytask);
+
+    settitle('')
+  };
+
+  const clickHandler = (e,i) =>{
+     const copytask = [...tasks]
+     copytask[i].completed = !copytask[i].completed;
+     settasks(copytask)
+  };
+
+  const deleteHandler = (e,i) =>{
+    const copytask = [...tasks]
+    copytask.splice(i,1);
+    settasks(copytask)
+  }
+  let renderTasks = (
+    <h1 className="text-3xl mt-10 text-white text-center font-semibold">No Pending Task</h1>
+  );
+
+
+  if(tasks.length > 0){
+    renderTasks = tasks.map((task,index) => (
+         <li key={index} className="mb-5 flex justify-between items-center border border-orange-300 rounded-full p-5">
+            <div className="flex items-center">
+              <div
+              onClick={(e)=>{clickHandler(e,index)}}
+                className={`${task.completed ? "bg-green-600" : "border-2"} mr-4 rounded-full w-[30px] h-[30px] border-orange-600 `}
+              ></div>
+              <h1 className={`${task.completed ? "line-through" : "" } text-2xl font-extrabold text-yellow-100`}>
+                {task.title}
+              </h1>
+            </div>
+            <div className="flex gap-3 text-2xl text-yellow-100">
+              <i className="ri-file-edit-line"></i>
+              <i onClick={(e)=>{deleteHandler(e,index)}} className="ri-delete-bin-3-line"></i>
+            </div>
+          </li>
+    ));
+  }
   return (
-   
-   <div className="mt-[7%] w-[25%] h-[20%] flex rounded-3xl border justify-around items-center ">
-     <div className="text-yellow-100">
-      <h1 className="text-3xl font-bold">Let's ToDo</h1>
-      <p>keep doing things</p>
-     </div>
-     <form className="w-[25%] flex px-5 my-[2%]">
-      <input type="text" placeholder="your task here..." name="task" className="px-5 py-3 text-yellow-100 outline-none w-[85%] rounded-xl bg-zinc-700" />
-      <button className="outline-none text-4xl font-extrabold flex justify-center items-center w-[50px] h-[50px] rounded-full bg-orange-600">+</button>
-     </form>
-     <ul>
-      <li>
-        <div>
-          <h3>taskName</h3>
+    <>
+      <div className="w-screen  h-screen flex flex-col items-center justify-center  bg-zinc-800">
+        <div className="w-[37%] h-[20%] rounded-xl p-10 border-2 border-orange-600">
+          <div className="flex w-full h-full justify-between items-center">
+            <h1 className="font-extrabold text-3xl text-white">Let's ToDo</h1>
+            <div className="w-[30%] h-22  flex items-center justify-center rounded-full bg-orange-600">
+              <h2 className=" font-extrabold text-4xl text-white py-5">{tasks.filter(task => task.completed).length}/{tasks.length}</h2>
+            </div>
+          </div>
         </div>
-      </li>
-     </ul>
-     <div>
-      0/0
-     </div>
+        <form onSubmit={submitHandler} className="my-4 flex gap-6">
+          <input
+            onChange={(e) => {
+              settitle(e.target.value);
+            }}
+            value={title}
+            className="outline-none bg-transparent border-2 text-white px-6 py-2 rounded-md placeholder:text-white"
+            type="text"
+            placeholder="Task Name..."
+          />
+          <button className="bg-orange-600 px-4 py-3 rounded-full text-xl text-white">
+            Create
+          </button>
+        </form>
+        <ul className="w-[35%] h-1/3">
+          {renderTasks}
+        </ul>
+      </div>
+    </>
+  );
+};
 
-
-
-   </div>
-   
-  )
-}
-
-export default App
+export default App;
